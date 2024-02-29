c5a02bd1fa8482748d8f6570989ce34bedd71dbc
dayvidtp <dayvid.pinto0@gmail.com>
2024-02-29 18:13:31 -0300

    add-cards-using-JavaScript

---
+++ src/scripts/engine.js
@@ -0,0 +1,26 @@
+const emojis = [
+    "🤣",
+    "🤣",
+    "😊",
+    "😊",
+    "❤",
+    "❤",
+    "😍",
+    "😍",
+    "👌",
+    "👌",
+    "👍",
+    "👍",
+    "🤞",
+    "🤞",
+];
+let openCards = [];
+
+let shuffleEmojis = emojis.sort(() =>(Math.random() > 0.5 ? 2 : -1));
+
+for(let i=0; i < emojis.length; i++) {
+    let box = document.createElement("div");
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    document.querySelector(".game").appendChild(box);
+}
